779913889b82ce27829486f29416e0c11e445a93
Pranali Khambale <pranalisk1001@gmail.com>
2025-06-30 23:47:20 +0530

    add new line

--- app.js
+++ app.js
@@ -77,6 +77,7 @@ app.use((req,res,next) => {
   res.locals.currUser = req.user ||null;
   next();
 });
+app.get("/favicon.ico", (req, res) => res.status(204).end());
 
 // app.get("/demouser",async (req,res) => {
 //   let fakeUser = new User({
